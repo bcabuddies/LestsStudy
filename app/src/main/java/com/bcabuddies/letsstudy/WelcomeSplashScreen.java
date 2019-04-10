8969c403c908f28913d0cbe39b5272ea61ea2a90
Rahul Gaur <rahul.gaur152@gmail.com>
2019-04-11 04:05:24 +0530

    Added splash screen in starting of the App

---
+++ app/src/main/java/com/bcabuddies/letsstudy/WelcomeSplashScreen.java
@@ -0,0 +1,30 @@
+package com.bcabuddies.letsstudy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.bcabuddies.letsstudy.Home.view.MainActivity;
+
+public class WelcomeSplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_welcome_splash_screen);
+
+        //show splash screen
+        int SPLASH_TIME_OUT = 1000;
+        new Handler().postDelayed(() -> {
+            Intent welcomeIntent = new Intent(WelcomeSplashScreen.this, MainActivity.class);
+            startActivity(welcomeIntent);
+            finish();
+        }, SPLASH_TIME_OUT);
+    }
+}
